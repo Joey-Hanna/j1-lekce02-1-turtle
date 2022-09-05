5be71737c89f25fcbbadbd3aac2eb20e514d4876
Joey-Hanna <joey.martin.de.lange@gmail.com>
2022-09-05 21:53:17 +0200

    zelva zofka kresli nekolik domecku u sebe

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -8,6 +8,44 @@ public class HlavniProgram {
         Turtle zofka;
 
         zofka = new Turtle();
+
+
+
+        for (int j = 0; j < 3; j++) {
+            for (int i = 0; i < 5; i++) {
+                zofka.move(150);
+                zofka.turnRight(90);
+            }
+
+            zofka.turnLeft(45);
+            zofka.move(100);
+            zofka.turnRight(45);
+            zofka.turnRight(45);
+            zofka.move(100);
+
+            zofka.penUp();
+            zofka.turnRight(45);
+            zofka.move(150);
+            zofka.turnLeft(90);
+            zofka.move(50);
+            zofka.turnLeft(90);
+
+            zofka.penDown();
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 }
